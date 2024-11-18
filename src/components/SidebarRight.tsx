1ec3192bb10c78b32adb30e0f1e5634a39e2f057
Parth Govale <133950391+Demolus13@users.noreply.github.com>
2024-11-18 16:09:43 +0530

    feat: update SidebarRight component with new timeline entries and improved layout

--- src/components/SidebarRight.tsx
+++ src/components/SidebarRight.tsx
@@ -26,14 +26,18 @@ const timelineData: Timeline[] = [
     label: "Open Manipulator X",
     timelines: [
       {
-        href: "llm-pick-and-place",
-        title: "LLM Pick and Place",
+        href: "calibration-workspace",
+        title: "Calibration Workspace",
+      },
+      {
+        href: "define-color-bounds",
+        title: "Define Color Bounds",
       },
     ],
   },
 ];
 
-export default function SidebarRight({}: Props) {
+export default function SidebarRight({ }: Props) {
   const [isSidebar, setSidebar] = useState(true);
 
   function toggleSidebar() {
@@ -48,8 +52,29 @@ export default function SidebarRight({}: Props) {
       })}
     >
       {isSidebar && (
-        <div className="min-h-screen w-full pl-4 pr-6 pt-20 dark:bg-[#0D0D0D]">
-          {/* Fill this section */}
+        <div
+          className={cn(
+            "min-h-screen   w-full  pl-4 pr-6 pt-20 dark:bg-[#0D0D0D]   "
+          )}
+        >
+          {/* new chat btn */}
+          <div className="absolute top-5 left-0 pl-4 pr-6 w-full ">
+            <Link
+              href={"/"}
+              className="flex  dark:bg-[#0D0D0D] justify-between w-full  items-center p-2 hover:bg-slate-800 rounded-lg transition-all "
+            >
+              <section className="flex items-center gap-2">
+                <p className="text-sm">LLM Pick and Place</p>
+              </section>
+            </Link>
+          </div>
+
+          {/* timeles */}
+          <div className="w-full flex flex-col gap-5">
+            {timelineData.map((d, i) => (
+              <Timeline key={i} label={d.label} timelines={d.timelines} />
+            ))}
+          </div>
         </div>
       )}
       <div className="absolute inset-y-0 left-[-30px] flex items-center justify-center w-[30px]">
@@ -69,23 +94,18 @@ function Timeline(props: Timeline) {
   const pathName = usePathname();
 
   return (
-    <div className="w-full flex flex-col">
-      <p className="text-sm text-gray-500 font-bold p-2">{props.label}</p>
+    <div className="w-full flex flex-col gap-2">
       {props.timelines.map((d, i) => (
         <Link
           key={i}
           className={cn(
-            "p-2 group ease-in-out duration-300 hover:bg-slate-800 rounded-lg transition-all items-center text-sm w-full flex justify-between",
+            "p-2 group ease-in-out duration-300 bg-slate-900 hover:bg-slate-800 rounded-lg transition-all items-center text-sm w-full flex justify-between",
             { "bg-slate-800": `/${d.href}` === pathName }
           )}
           href={d.href}
         >
           <div className="text-ellipsis overflow-hidden w-[80%] whitespace-nowrap">
             {d.title}
-          </div>
-          <div className="transition-all items-center gap-2 hidden group-hover:flex ease-in-out duration-300">
-            <BsThreeDots />
-            <BsArchiveFill />
           </div>
         </Link>
       ))}
